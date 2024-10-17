13a5222d4ae573ceff61c90a5801433f0253b232
Jeriel Benavides <jerielmanuelb@outlook.com>
2024-10-17 01:27:06 -0400

    Added about self-description

--- resources/js/Pages/Pages/Components/About.jsx
+++ resources/js/Pages/Pages/Components/About.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const About = () => {
     return (
         <section id="about" className="py-20">
-            <h2 className="text-3xl font-bold text-center">About Me</h2>
-            <p className="mt-4 max-w-2xl mx-auto text-center">
-                [Placeholder: A brief introduction about me.]
+            <h2 className="text-3xl font-bold">About Me</h2>
+            <p className="mt-4 mx-auto text-justify text-white text-lg">
+                Hi, I’m Jeriel Benavides, a driven <strong>Software Engineer</strong> and Full Stack Web Developer based in Pickering, Ontario. With extensive experience in crafting cutting-edge, scalable applications, I excel at creating intuitive front-end experiences using modern frameworks like Vue and React, while building powerful, efficient back-end systems with Laravel. My expertise lies in transforming complex, high-stakes challenges into streamlined, impactful solutions that deliver real value. Whether it's architecting an intricate system or refining the user experience, I bring a relentless focus on quality and innovation to every project.
             </p>
         </section>
     );
